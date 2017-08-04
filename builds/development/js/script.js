d1999cc53a92da21660701fe3b036c3e1c5f3d79
Patrick McSweeney <patrickmcsweeney@tecace.com>
2017-08-04 15:25:39 -0700

    Concetenated JS files

---
+++ builds/development/js/script.js
@@ -0,0 +1,9 @@
+var script1 = function() {
+	console.log("script1 loaded");
+}();
+var script2 = function() {
+	console.log("script2 loaded");
+}();
+var script3 = function() {
+	console.log("script3 loaded");
+}();
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
 
-var gutil = require('gulp-util');
+var gutil = require('gulp-util'),
+	gconcat = require('gulp-concat');
 
 gulp.task('log', function() {
 	gutil.log("Workflow generate: START");
+});
+
+var jsSources = ['components/scripts/script1.js','components/scripts/script2.js','components/scripts/script3.js'];
+
+gulp.task('process-js', function() {
+	gulp.src(jsSources)
+		.pipe(gconcat('script.js'))
+		.pipe(gulp.dest('builds/development/js'));
 });
