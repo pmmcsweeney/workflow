5a2b123d0c109c97b665f86a307260d5850cafac
Patrick McSweeney <patrickmcsweeney@tecace.com>
2017-08-04 15:42:14 -0700

    added sass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,30 @@ var gulp = require('gulp');
 
 var gutil = require('gulp-util'),
 	gconcat = require('gulp-concat'),
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify')
+	compass = require('gulp-compass');
 
 gulp.task('log', function() {
 	gutil.log("Workflow generate: START");
 });
 
 var jsSources = ['components/scripts/script1.js','components/scripts/script2.js','components/scripts/script3.js'];
+var sassSources = ['components/sass/style.scss'];
 
-gulp.task('process-js', function() {
+gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(gconcat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('sass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
 });
